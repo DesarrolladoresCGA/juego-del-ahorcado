99fcbb0a062d619d4a47b2599e6f0aa8a760cc37
Luis Bonilla <luisantoniob22@gmail.com>
2022-09-13 11:56:59 -0500

    funcion de trnacicion

--- js/main.js
+++ js/main.js
@@ -2,33 +2,47 @@
 
     var juego ={
         palabra: 'ALURA',
-        estado: 1,
+        estado: 7,
         adivinando:['A', 'L'],
         errado: ['B', 'J', 'K', 'C']
     }
     var $html ={
         hombre: document.getElementById('img-Hombre'),
-        linea : document.getElementById('lienas'),
         adivinando: document.querySelector('.adivinado'),
         errado: document.querySelector('.errado')
     } 
 
-    //function dibujar(juego){
+    function dibujar(juego){
         //Actualizar la imagen del ahorcado
         var $elem
         $elem = $html.hombre;
-        $elem.src ="../img/estado " + juego.estado + ".png";
+
+        var estado = juego.estado;
+
+        if(estado === 8){
+
+            estado = juego.previo;
+        }
+
+        $elem.src ="../img/estado " + estado + ".png";
 
          //Creamos las letras adivinadas
         var palabra = juego.palabra;
         var adivinando = juego.adivinando;
         $elem = $html.adivinando;
+
+        //borrar los elementos anteriores
+        $elem.innerHTML ='';
+
         for (let letra of palabra){
+
             let $span = document.createElement('span');
             let $txt = document.createTextNode('');
+
             if(adivinando.indexOf(letra) >= 0){
                 $txt.nodeValue = letra;
             }
+
             $span.setAttribute('class','letra adivinada');
             $span.appendChild($txt);
             $elem.appendChild($span);
@@ -36,7 +50,11 @@
         //creamos las letras erraas
         var errado = juego.errado;
         $elem = $html.errado;
+        //borrar los elementos anteriores
+        $elem.innerHTML ='';
+
         for(let letra of errado){
+
             let $span = document.createElement('span');
             let $txt = document.createTextNode(letra);
             $span.setAttribute('class','errado')
@@ -44,8 +62,55 @@
             $elem.appendChild($span);
 
         }
+    }
+    function adivinar(juego, letra){
 
-    //}
-    console.log(juego);
+        let estado = juego.estado;
 
+        if(estado === 1 || estado === 8){
+            return;
+        }
+
+        var adivinando = juego.adivinando;
+        var errado = juego.errado;
+
+        if(adivinando .indexOf(letra) >= 0 || errado.indexOf(letra) >= 0){
+            return;
+        }
+
+        var palabra = juego.palabra;
+
+        if(palabra.indexOf(letra) >=0){
+            let ganado = true;
+            for(let l of palabra){
+                if(adivinando.indexOf(l) < 0 && l  != letra){
+                    ganado = false;
+                    juego.previo = juego.estado;
+                    break;
+                }
+            }
+            if(ganado){
+                juego.estado = 8;
+            }
+            adivinando.push(letra);
+        }else{
+            juego.estado--;
+            errado.push(letra);
+        }
+    }
+
+    window.onkeypress = function adivinarLetra(e){
+        var letra = e.key;
+
+        ///convertir letra minuscula en mayuscula
+        letra = letra.toUpperCase();
+        //chequear que sea una letra valida
+        if(!/[*A-ZÑ]/.test(letra)){
+            return;
+        }
+        adivinar(juego, letra);
+        dibujar(juego);
+    }
+
+    dibujar(juego);
 }());
